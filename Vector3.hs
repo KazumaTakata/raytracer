bda052d7fe8a8b4e9f0d3528789fda8035009a57
Kazuma Takata <neilbuzztakata@gmail.com>
2019-10-05 06:49:23 +0900

    normal viewer

--- Vector3.hs
+++ Vector3.hs
@@ -15,6 +15,9 @@ sub (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 - x2) (y1 - y2) (z1 - z2)
 elementMul :: Vec3 -> Float -> Vec3
 elementMul (Vec3 x y z) scala = Vec3 (x * scala) (y * scala) (z * scala)
 
+dot :: Vec3 -> Vec3 -> Float
+dot (Vec3 x y z) (Vec3 x1 y1 z1) = x * x1 + y * y1 + z * z1
+
 lengthOf :: Vec3 -> Float
 lengthOf (Vec3 x y z) = sqrt (x * x + y * y + z * z)
 
--- main.hs
+++ main.hs
@@ -17,14 +17,39 @@ data Screen =
          Horizontal
          Vertical
 
+hitSphere :: Vector3.Vec3 -> Float -> Ray -> Float
+hitSphere center radius (Ray ori dir) =
+  let oc = Vector3.sub ori center
+      a = Vector3.dot dir dir
+      b = 2.0 * Vector3.dot oc dir
+      c = (Vector3.dot oc oc) - (radius * radius)
+      disc = b * b - 4 * a * c
+   in if (disc < 0)
+        then -1
+        else (-b - sqrt (disc)) / (2 * a)
+
 getColor :: Ray -> RGB
 getColor (Ray ori dir) =
   let unit_dir = Vector3.makeUnitVector dir
-      t = 0.5 * ((Vector3.y unit_dir) + 1.0)
+      dir_y = 0.5 * ((Vector3.y unit_dir) + 1.0)
+      center = Vector3.Vec3 0 0 (-1)
+      t = hitSphere center 0.5 (Ray ori dir)
       vec3 =
-        Vector3.add
-          (Vector3.elementMul (Vector3.Vec3 1 1 1) (1.0 - t))
-          (Vector3.elementMul (Vector3.Vec3 0.5 0.7 1.0) t)
+        if t > 0
+          then let n =
+                     Vector3.makeUnitVector
+                       (Vector3.sub
+                          (pointAtParameter (Ray ori dir) t)
+                          (Vector3.Vec3 0 0 (-1)))
+                   nx = (Vector3.x n)
+                   ny = (Vector3.y n)
+                   nz = (Vector3.z n)
+                in Vector3.elementMul
+                     (Vector3.Vec3 (nx + 1) (ny + 1) (nz + 1))
+                     0.5
+          else Vector3.add
+                 (Vector3.elementMul (Vector3.Vec3 1 1 1) (1.0 - dir_y))
+                 (Vector3.elementMul (Vector3.Vec3 0.5 0.7 1.0) dir_y)
    in RGB
         (round (255 * (Vector3.x vec3)))
         (round (255 * (Vector3.y vec3)))
