eb1abe477a62c2f52eafb4888aad3484548c6351
Kazuma Takata <neilbuzztakata@gmail.com>
2019-10-06 08:17:29 +0900

    ch5

--- Hitable.hs
+++ Hitable.hs
@@ -12,14 +12,16 @@ type Normal = Vector3.Vec3
 
 type HitPoint = Vector3.Vec3
 
-data Hit_record =
-  Hit_record Float
-             Normal
-             HitPoint
+data Hit_record = Hit_record
+  { t :: Float
+  , normal :: Normal
+  , hit_point :: HitPoint
+  }
 
-data HitRecordAndBool =
-  HitRecordAndBool Hit_record
-                   Bool
+data HitRecordAndBool = HitRecordAndBool
+  { hit_record :: Hit_record
+  , isHit :: Bool
+  }
 
 class Hitable a where
   hit :: a -> Ray -> Min -> Max -> HitRecordAndBool
@@ -36,10 +38,11 @@ instance Hitable Shpere where
   hit (Shpere center radius) (Ray ori dir) min max =
     let oc = Vector3.sub ori center
         a = Vector3.dot dir dir
-        b = 2.0 * Vector3.dot oc dir
+        b = Vector3.dot oc dir
         c = (Vector3.dot oc oc) - (radius * radius)
         disc = b * b - a * c
         dammy = Vector3.Vec3 0 0 0
+        falsedata = HitRecordAndBool (Hit_record 0 dammy dammy) False
      in if (disc > 0)
           then let tmp = (-b - sqrt (disc)) / (a)
                 in if (tmp < max) && (tmp > min)
@@ -62,13 +65,22 @@ instance Hitable Shpere where
                                       in HitRecordAndBool
                                            (Hit_record tmp normal point)
                                            True
-                                else HitRecordAndBool
-                                       (Hit_record 0 dammy dammy)
-                                       False
-          else HitRecordAndBool (Hit_record 0 dammy dammy) False
+                                else falsedata
+          else falsedata
 
 data HitableList a =
   HitableList [a]
-              Int
 
-iterateHitable :: (Hitable a) => HitableList a -> HitRecordAndBool
+iterateHitable ::
+     (Hitable a) => HitableList a -> Ray -> Min -> Max -> HitRecordAndBool
+iterateHitable (HitableList (x:[])) ray min max = hit x ray min max
+iterateHitable (HitableList (x:xs)) ray min max =
+  let hitrecordandbool = hit x ray min max
+      newmax =
+        if isHit hitrecordandbool
+          then t (hit_record hitrecordandbool)
+          else max
+      accHit = iterateHitable (HitableList xs) ray min newmax
+   in if isHit accHit
+        then accHit
+        else hitrecordandbool
--- main.hs
+++ main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Hitable
 import ImageData
 import Ray
 import qualified Vector3
@@ -17,30 +18,14 @@ data Screen =
          Horizontal
          Vertical
 
-hitSphere :: Vector3.Vec3 -> Float -> Ray -> Float
-hitSphere center radius (Ray ori dir) =
-  let oc = Vector3.sub ori center
-      a = Vector3.dot dir dir
-      b = 2.0 * Vector3.dot oc dir
-      c = (Vector3.dot oc oc) - (radius * radius)
-      disc = b * b - 4 * a * c
-   in if (disc < 0)
-        then -1
-        else (-b - sqrt (disc)) / (2 * a)
-
-getColor :: Ray -> RGB
-getColor (Ray ori dir) =
+getColor :: (Hitable a) => Ray -> HitableList a -> RGB
+getColor (Ray ori dir) hitablelist =
   let unit_dir = Vector3.makeUnitVector dir
       dir_y = 0.5 * ((Vector3.y unit_dir) + 1.0)
-      center = Vector3.Vec3 0 0 (-1)
-      t = hitSphere center 0.5 (Ray ori dir)
+      hitrecordandbool = iterateHitable hitablelist (Ray ori dir) 0 200000
       vec3 =
-        if t > 0
-          then let n =
-                     Vector3.makeUnitVector
-                       (Vector3.sub
-                          (pointAtParameter (Ray ori dir) t)
-                          (Vector3.Vec3 0 0 (-1)))
+        if isHit hitrecordandbool
+          then let n = normal (hit_record hitrecordandbool)
                    nx = (Vector3.x n)
                    ny = (Vector3.y n)
                    nz = (Vector3.z n)
@@ -59,26 +44,45 @@ type X = Float
 
 type Y = Float
 
-renderAt :: X -> Y -> RGBData -> Origin -> Screen -> RGBData
-renderAt x y (RGBData list) ori screen =
+renderAt ::
+     (Hitable a)
+  => X
+  -> Y
+  -> RGBData
+  -> Origin
+  -> Screen
+  -> HitableList a
+  -> RGBData
+renderAt x y (RGBData list) ori screen hitablelist =
   let u = (x / 200.0)
       v = (y / 100.0)
       dir = genDirection screen u v
       ray = Ray ori dir
-      color = getColor ray
+      color = getColor ray hitablelist
    in RGBData (list ++ [color])
 
-renderCol :: Y -> RGBData -> Origin -> Screen -> RGBData
-renderCol 0 rgbdata ori screen = renderRow 0 0 rgbdata ori screen
-renderCol y rgbdata ori screen =
-  let newrgbdata = renderRow 0 y rgbdata ori screen
-   in renderCol (y - 1) newrgbdata ori screen
+renderCol ::
+     (Hitable a) => Y -> RGBData -> Origin -> Screen -> HitableList a -> RGBData
+renderCol 0 rgbdata ori screen hitablelist =
+  renderRow 0 0 rgbdata ori screen hitablelist
+renderCol y rgbdata ori screen hitablelist =
+  let newrgbdata = renderRow 0 y rgbdata ori screen hitablelist
+   in renderCol (y - 1) newrgbdata ori screen hitablelist
 
-renderRow :: X -> Y -> RGBData -> Origin -> Screen -> RGBData
-renderRow 199 y rgbdata ori screen = renderAt 199 y rgbdata ori screen
-renderRow x y rgbdata ori screen =
-  let newrgbdata = renderAt x y rgbdata ori screen
-   in renderRow (x + 1) y newrgbdata ori screen
+renderRow ::
+     (Hitable a)
+  => X
+  -> Y
+  -> RGBData
+  -> Origin
+  -> Screen
+  -> HitableList a
+  -> RGBData
+renderRow 199 y rgbdata ori screen hitablelist =
+  renderAt 199 y rgbdata ori screen hitablelist
+renderRow x y rgbdata ori screen hitablelist =
+  let newrgbdata = renderAt x y rgbdata ori screen hitablelist
+   in renderRow (x + 1) y newrgbdata ori screen hitablelist
 
 genDirection :: Screen -> Float -> Float -> Vector3.Vec3
 genDirection (Screen low hor ver) u v =
@@ -97,7 +101,10 @@ main = do
   let origin = Vector3.Vec3 0 0 0
   let screen = genScreen
   let initrbgdata = RGBData []
-  let rbgdata = renderCol 99 initrbgdata origin screen
+  let sphere1 = Shpere (Vector3.Vec3 0 0 (-1)) 0.5
+  let sphere2 = Shpere (Vector3.Vec3 0 (-100.5) (-1)) 100
+  let hitablelist = HitableList [sphere1, sphere2]
+  let rbgdata = renderCol 99 initrbgdata origin screen hitablelist
   let ppmFile = PPMFile 200 100 rbgdata
   let imagedata = writePPM ppmFile
   writeFile "sample.ppm" imagedata
